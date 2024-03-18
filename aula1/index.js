e4e7cf602b1163fa8b3c8bdd3027abc2efbcb18c
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-03-18 18:25:02 -0300

    estudos do dia 18/03/24

---
+++ aula1/index.js
@@ -0,0 +1,6 @@
+console.log(`Hello World!`)
+console.log('Marlon Bezerra', 'Bezerra Marlon')
+console.log(19)
+console.log(0.47, 400.39)
+//
+console.log('Meu nome é "Marlon Bezerra dos Santos". Estou aprendendo JavaScript ás', 16, 'da tarde!')
---
+++ aula2/index.js
@@ -0,0 +1,14 @@
+//Comentários de linha
+
+//console.log("Hello World");
+console.log('Olá mundo!');
+console.log(23.19)//minha conta bancária :(
+///console.log("Marlon sou eu");
+
+//Comentários de bloco 
+
+/*
+console.log('Marlob é meu nickname');
+console.log('Gosto de jogar e ler mangás');
+console.log('Acho que já deu de console.log :/')
+*/
