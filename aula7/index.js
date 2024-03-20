cc2b2599851039645e797fd93d0a96b00002b3a9
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-03-20 18:31:30 -0300

    estudos do dia 20/03/24

---
+++ aula7/index.js
@@ -0,0 +1,27 @@
+// Variáveis Javascript
+
+     // Strings
+
+const nome = 'Marlon';
+const nome2 = "Bezerra";
+const nome3 = `Santos`;
+
+     // Number
+
+const idade = 19;
+const salario = 1200.05;
+
+     // Undefined
+
+let nomeAluno;
+
+     // Null
+
+const sobrenomeAluno = null;
+
+     // Booleano
+
+let aprovado;
+aprovado = true;
+aprovado = false;
+
---
+++ aula8/index.js
@@ -0,0 +1,26 @@
+const num1 = 20;
+const num2 = 2;
+const num3 = 7;
+
+let result = num1 + num2 + num3;
+result = num1 - num2 - num3;
+result = num2 * num3;
+result = num1 / num3;
+result = num1 % num3;
+result = num2 ** num1;
+result = 0;
+++result;
+--result;
+result = 10;
+result += 2;
+result -= 4;
+result *= 10;
+result /= 20;
+result **= 2;
+result %= 3;
+result = false;
+result = true;
+result = 10;
+result = 10 * ((2 + 4) / 5 * 8)
+
+console.log(result);
