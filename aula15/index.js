d758ae4ebd0843688110663172adc27d65e746b6
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-05-07 17:22:16 -0300

    estudos do dia 07/05/24

--- aula15/index.js
+++ aula15/index.js
@@ -1,11 +1,21 @@
-function criaPessoa(nome, idade) {
-    return {
-        nome:nome, 
-        idade:idade
+// function criaPessoa(nome, idade) {
+//     return {
+//         nome:nome, 
+//         idade:idade
+//     }
+// }
+
+
+// pessoa1 = criaPessoa('Marlon', 20);
+// console.log(pessoa1.nome);
+// console.log(pessoa2.nome);
+
+const pessoa = {
+    nome: 'Marlon',
+    idade: 19,
+    idadePosterior: function(x) {
+        return pessoa.idade + x;
     }
-}
+};
 
-
-pessoa1 = criaPessoa('Marlon', 20);
-console.log(pessoa1.nome);
-console.log(pessoa2.nome);
+console.log(pessoa.idadePosterior(10));
