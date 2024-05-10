3dc027da55a5e820784c4fc0107209b2be340a16
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-05-10 17:21:37 -0300

    estudos dia 10/05/24

--- aula16/index.js
+++ aula16/index.js
@@ -1,15 +1,15 @@
-// let nome = 'Marlon';
-// nome[5] = 'b';
-// console.log(nome);
+let nome = 'Marlon';
+nome[5] = 'b';
+console.log(nome);
 
-// let a = 'a'; //declaramos uma variavel com o valor "a"
-// let b = a; //agora a variavel "b" recebeu uma CÓPIA de "a"
-// a = 'c' //e então "a" recebeu outro valor
-// console.log(a, b) // ao exibirmos temos que as variáveis "A" e "B" tem são diferentes e não estão ligados
+let a = 'a'; //declaramos uma variavel com o valor "a"
+let b = a; //agora a variavel "b" recebeu uma CÓPIA de "a"
+a = 'c' //e então "a" recebeu outro valor
+console.log(a, b) // ao exibirmos temos que as variáveis "A" e "B" tem são diferentes e não estão ligados
 
-// const nomes = ['Marlon', 'José', 'Geane', 'João']
-// nomes[0] = 'Giovana';
-// console.log(nomes)
+const nomes = ['Marlon', 'José', 'Geane', 'João']
+nomes[0] = 'Giovana';
+console.log(nomes)
 
 const nomes = ['Marlon', 'João', 'José', 'Geane'];  
 const novoNomes = nomes;
---
+++ aula17/script/index.js
@@ -0,0 +1,2 @@
+const nome  = document.getElementById('nome');
+
