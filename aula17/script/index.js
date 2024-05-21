52c2b8a1a816da533201a5c0a8c9497f1d5e4905
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-05-21 18:00:02 -0300

    estudos do dia 21/05/24

--- aula17/script/index.js
+++ aula17/script/index.js
@@ -1,2 +1,27 @@
-const nome  = document.getElementById('nome');
+const objetos = [];
+function meuEscopo() {
+    let form = document.querySelector('.form'); 
+    function eventoListener (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const altura = form.querySelector('.altura');
+        const peso = form.querySelector('.peso'); 
 
+        const resultado = document.querySelector('.resultado');
+        resultado.innerHTML += `<p>${nome.value}, ${sobrenome.value}, ${altura.value}, ${peso.value}</p>`;
+        
+        function criaObjetos (nome, sobrenome, altura, peso) {
+            return {
+                nome, 
+                sobrenome, 
+                altura, const lista = ['YA', 'BISH'];
+                peso
+            }
+        };
+        objetos.push(criaObjetos(nome.value, sobrenome.value, altura.value, peso.value));
+        console.log(objetos);
+    };
+    form.addEventListener('submit', eventoListener);
+}
+meuEscopo();
