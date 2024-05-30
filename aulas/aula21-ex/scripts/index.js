bf013d9ede9c3c61ff8ce399733e941e795fe4c5
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-05-30 17:57:27 -0300

    estudos do dia 30/05/24

--- aulas/aula21-ex/scripts/index.js
+++ aulas/aula21-ex/scripts/index.js
@@ -1,29 +1,31 @@
 const send = document.querySelector(".input-send"); 
-
-send.addEventListener("click", function(event) { //anotar isso!!
+send.addEventListener("click", function(event) {
     event.preventDefault(event);
-    
-    const peso = document.querySelector(".peso");
-    const altura = document.querySelector(".altura");
-    const imc = Number(peso.value/ (altura.value * altura.value)).toFixed(1);
+    let peso = document.querySelector(".peso");
+    peso = Number(peso.value)
+    console.log(peso, typeof(peso))
+    let altura = document.querySelector(".altura");
+    altura = Number(altura.value);
+    console.log(altura, typeof(altura))
     const resultado = document.querySelector(".resultado");
-    console.log(imc, peso.value, altura.value)
-
-    if (peso.value === NaN || peso.value === undefined || peso.value === 0) {
-        resultado.innerHTML = `<p>Peso inválido</p>`;
-    } else if (altura.value === NaN || altura.value === undefined || altura.value === 0) {
-        resultado.innerHTML = `<p>Altura inválida</p>`;
-    } else if (imc < 18.5) {
-        resultado.innerHTML = `<p style="padding: 2%"; >Seu IMC é de ${imc} (Abaixo do peso)</p>` ;
-    } else if (imc >= 18.5 && imc <= 24.9) {
-        resultado.innerHTML = `<p style="padding: 2%;" >Seu IMC é de ${imc} (Peso normal)</p>`;
-    } else if (imc >= 25 && imc <= 29.9) {
-        resultado.innerHTML = `<p style="padding: 2%;" >Seu IMC é de ${imc} (Sobrepeso)</p>`;
-    } else if (imc >= 30 && imc <= 34.9) {
-        resultado.innerHTML = `<p style="padding: 2%;" >Seu IMC é de ${imc} (Obesidade grau 1)</p>`;
-    } else if (imc >= 35 && imc <= 39.9) {
-        resultado.innerHTML = `<p style="padding: 2%;" >Seu IMC é de ${imc} (Obesidade grau 2)</p>`;
-    } else if (imc > 40) {
-        resultado.innerHTML = `<p style="padding: 2%;" >Seu IMC é de ${imc} (Obesidade grau 3)</p>`;
+    if (peso === 0) {
+        resultado.innerHTML = `<p style="padding: 2%; background-color: rgba(255, 0, 0, 0.568); border: 2px solid black;">Peso inválido!</p>`
+    } else if (altura === 0) {
+        resultado.innerHTML = `<p style="padding: 2%; background-color: rgba(255, 0, 0, 0.568); border: 2px solid black;">Altura inválida!</p>`
+    } else {
+        const imc = (peso / (altura * altura)).toFixed(2);
+        if (imc < 18.5) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Abaixo do peso)</p>`;
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Peso normal)</p>`;
+        } else if (imc >= 25 && imc <= 29.9) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Sobrepeso)</p>`;
+        } else if (imc >= 30 && imc <= 34.9) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Obesidade grau 1)</p>`;
+        } else if (imc >= 35 && imc <= 39.9) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Obesidade grau 2)</p>`;
+        } else if (imc > 40) {
+            resultado.innerHTML = `<p style="padding: 2%; border: 2px solid black;" >Seu IMC é de <strong>${imc}</strong> (Obesidade grau 3)</p>`;
+        }
     }
 })
