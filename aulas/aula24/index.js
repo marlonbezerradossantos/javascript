d1d9afc8d97ccf24d857cec24a5e19f4cdaacf3a
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-06-20 18:18:13 -0300

    estudos do dia 20/06/24

--- aulas/aula24/index.js
+++ aulas/aula24/index.js
@@ -1,2 +1,10 @@
-const data = new Date(1, 6);
-console.log(data.toString());
+const data = new Date();
+data.setFullYear(2005);
+data.setMonth(7);
+data.setDate(6)
+data.setHours(9);
+data.setMinutes(43);
+data.setSeconds(2);
+data.setMilliseconds(2)
+
+console.log(data.toString(), data.getMilliseconds());  
---
+++ aulas/aula25/index.js
@@ -0,0 +1,13 @@
+const dia = new Date()
+
+switch (2023) {
+    case 2021:
+    case 2022:
+        console.log('Eu era menor de idade')
+        break;
+    case 2023:
+        console.log('Eu era maior de idade')
+        break;
+    default:
+        console.log('n sabemos')
+}
