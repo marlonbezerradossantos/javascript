28a0f48980b335bfb0cc366758e0cdd66e07c304
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-07-03 18:16:39 -0300

    estudos do dia 03/07/24

--- aulas/aula25/index.js
+++ aulas/aula25/index.js
@@ -11,18 +11,16 @@ const pessoa = {
 const {nome: n, sobrenome, idade, endereco: {rua, num}} = pessoa;
 console.log(n, sobrenome, idade, rua, num);
 
-// function display({ultimo, nome, sobrenome}) { // Aqui foi passado somente os "valores" do obj
-//     console.log(`Meu nome completo é ${nome} ${sobrenome} ${ultimo}. Prazer!"`)
-// }
+function display({ultimo, nome, sobrenome}) { // Aqui foi passado somente os "valores" do obj
+    console.log(`Meu nome completo é ${nome} ${sobrenome} ${ultimo}. Prazer!"`)
+}
 
+display(nomeCompleto);
 
+//  No caso dos objetos, nós podemos desestruturá-los nos argumentos de uma função, quando passamos um objeto como argumento a função busca no objeto passado os valores necessários para a execução através do nome das propriedades e o nome dos parâmetros pedidos, portanto o nome dos parâmetros devem ser passados entre chaves e ter seus nomes iguais para que a função retire seus valores para usá-los
 
-// display(nomeCompleto);
-
-// No caso dos objetos, nós podemos desestruturá-los nos argumentos de uma função, quando passamos um objeto como argumento a função busca no objeto passado os valores necessários para a execução através do nome das propriedades e o nome dos parâmetros pedidos, portanto o nome dos parâmetros devem ser passados entre chaves e ter seus nomes iguais para que a função retire seus valores para usá-los
-
-// const marcas = ["Natura", "O Boticário", "Avon"];
-// const [marca1, ...resto] = marcas;
-// console.log(marca1);
-// console.log(resto);
-// console.log(600/17)
+const marcas = ["Natura", "O Boticário", "Avon"];
+const [marca1, ...resto] = marcas;
+console.log(marca1);
+console.log(resto);
+console.log(600/17)
