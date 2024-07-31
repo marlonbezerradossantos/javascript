891277b927e5ae9c38129ba376a902f632e1cd86
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-07-31 19:01:36 -0300

    estudos do dia 31/07/24

--- exercicios/ex018/index.js
+++ exercicios/ex018/index.js
@@ -1,6 +1,10 @@
-const num = 4;
+const num = 200;
 try {
-    aalert(2)
+   //if(num > 5) throw 'Maior que 5'
+   if (!isNaN(num)) {
+    throw new Error('tem q ser maior q 5');
+   }
 } catch(err) {
     console.log(err)
+   
 }
