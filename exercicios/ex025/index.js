2bc0a9dc090a08d54a150e5b9bdb4313c0850f9d
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-08-21 02:54:29 -0300

    estudos do dia 21/08/24

--- exercicios/ex025/index.js
+++ exercicios/ex025/index.js
@@ -1,10 +1,28 @@
 // escreva uma função que receba uma string e retorne a quantidade de caracteres que se repetem mais de uma vez
 
 function contaDuplicados(str){
-    const quantidade = str.length
-    let semDuplos = new Set(str)
-    semDuplos = Array.from(semDuplos)
-    return quantidade - semDuplos.length
+    str = str.split('');
+    let letra = 0;
+    let duplicados = 0;
+    for(;letra < str.length; letra++) {
+        let cont = 0;
+        for(c of str) {
+            if(str[letra] === c && str[letra] !== null) {
+                cont++
+                console.log(cont)
+            } //? cont++ : cont += 0;
+        }
+        letra = 0;
+        if(cont >  1){
+            duplicados += 1
+            const x = str[letra]
+            for(i of str) {
+                if (i === x) i = null;
+            }
+        }
+        
+    }
+    return duplicados
 }
 
 console.log(contaDuplicados('aabbcccdefg'))
