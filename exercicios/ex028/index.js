6124194dd56c3edae079463ccb61905e5bed2be6
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-04 17:43:13 -0300

    estudos do dia 04/09/24

--- exercicios/ex028/index.js
+++ exercicios/ex028/index.js
@@ -1,19 +1,16 @@
-// function hexToRgb(r, g, b) {
-//     let list = [r, g, b]
-//     for(c in list) {
-//         if(list[c] < 0) {list[c] = list[c] * (-1) - list[c]}
-//         console.log(list[c])
-//     }
-//     console.log(list)
-//     return (r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0')).toUpperCase()
-// }
+//Escreva uma função que receba 3 valores em que compõem um código RGB e converta e retorne este valor em hexadecimal. Os valores passados devem estar sempre entre 0-255 e se caso não estiverem neste range, eles devem ser arredondados para o valor mais próximo. Devemos sempre ter 6 digitos HEX retornados.
 
-// console.log(hexToRgb( 0,   0, -20))
-
-let lista = [-1, -10, -20]
-for(c in lista) {
-    if(lista[c] < 0) {
-        lista[c] = lista[c] * (-1) - lista[c] 
+function hexToRgb(r, g, b) {
+    let list = [r, g, b]
+    for(c in list) {
+        if(list[c] < 0) {
+            list[c] = list[c] * (-1);
+            list[c] -= list[c]
+        } else if(list[c] > 255) {
+            const bckp = list[c] - 255
+            list[c] -= bckp
+        }
     }
+    return (list[0].toString(16).padStart(2, '0') + list[1].toString(16).padStart(2, '0') + list[2].toString(16).padStart(2, '0')).toUpperCase()
 }
-console.log(lista)
+console.log(hexToRgb( 256,   290, 400))
---
+++ exercicios/ex029/index.js
@@ -0,0 +1,13 @@
+// Escreva uma função que receberá 2 strings onde a primeira terá todas as suas letras convertidas para title-case(todas as primeiras letras maiusculas), e a segunda conterá outra string com palavras de exceção que terão todas as letras minusculas.
+
+function titleCase(str, min) {
+    min = min.split(' ')
+    
+
+    str = str.split(' ')
+    str.map()
+}
+
+titleCase('a clash of KINGS', 'a an the of')
+//titleCase('the quick brown fox')
+//titleCase('THE WIND IN THE WILLLOWS')
