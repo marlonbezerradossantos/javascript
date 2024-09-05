ba29ec30352e87ab379e6fa106e352bd24f9270b
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-05 17:56:43 -0300

    estudos do dia 05/06/24

--- exercicios/ex029/index.js
+++ exercicios/ex029/index.js
@@ -1,13 +1,30 @@
 // Escreva uma função que receberá 2 strings onde a primeira terá todas as suas letras convertidas para title-case(todas as primeiras letras maiusculas), e a segunda conterá outra string com palavras de exceção que terão todas as letras minusculas.
 
-function titleCase(str, min) {
-    min = min.split(' ')
-    
-
-    str = str.split(' ')
-    str.map()
+function titleCase(str, min = '#') {
+    console.log(min.length)
+    min = min.toLowerCase().split(' ');
+    str = str.toLowerCase().split(' ');
+    function maiuscula(e) {
+        for(c in e) {
+            e[c] = e[c].split('')
+            e[c][0] = e[c][0].toUpperCase()
+            e[c] = e[c].join("")
+        }
+        return e
+    }
+    maiuscula(str)
+    maiuscula(min)
+    for (c of min) {
+        const bckp = str.lastIndexOf(c)
+        if (bckp >= 1) {
+            str[bckp] = str[bckp].toLowerCase()
+        }
+    }
+    str = str.join(' ')
+    return str
 }
 
-titleCase('a clash of KINGS', 'a an the of')
+console.log(titleCase('the quick brown fox'))
+
 //titleCase('the quick brown fox')
 //titleCase('THE WIND IN THE WILLLOWS')
