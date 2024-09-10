11c28dcdd5dc83b9aa0ebf7d2a4a538fc5912a1f
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-10 18:02:48 -0300

    estudos do dia 10/09/24

--- exercicios/ex032/index.js
+++ exercicios/ex032/index.js
@@ -1,48 +1,35 @@
 // Escreva uma função que receba uma lista de arrays contendo intervalos entre dois números, retorne a soma destes interalos valores sobrepostos de intervalos devem ser contabilizados somente uma vez.
 
 function somaIntervalos(arr) {
-    /////
-    let cont = 0;
-    let soma = 0;
-    arr.map(function(e){
-        const bckp = e.pop()
-        for(i = e[0] + 1; i < bckp; i++) {
-            e.push(i)
+    let qtd = 0
+    items = arr.length
+    arr.map(function(x){//PREENCHE OS ARRAYS
+        const bckp = x.pop()
+        for(c = x[0] + 1; c < bckp; c++) {
+            x.push(c)
         }
-        e.push(bckp)
+        x.push(bckp)
     })
-
-    for(c of arr) {
-        soma += c[c.length - 1] - c[0]
-        //console.log('sou o arr:', c, '/// tenho essa soma', soma)
-    }
-    //console.log('eu', soma)
-    const newArr = [...arr[0], ...arr[1], ...arr[2]]
-    
-    for(c = 0; c < newArr.length; c++) {
-        if(newArr[c] > newArr[c + 1]) {
+    const newArr = [...arr[0], ...arr[1], ...arr[2]] //CRIA UM ARRAY UNICO COM TODOS OS NUMEROS
+    for(c = 0; c < newArr.length; c++) { //ITERA SOBRE TODOS OS ITENS
+        if(newArr[c] > newArr[c + 1]) { // PÕE OS ITENS EM ORDEM
             const bckp = newArr[c]
             newArr[c] = newArr[c + 1]
             newArr[c + 1] = bckp
             c = 0
         } 
-    }
-
-    for(c = 0; c < newArr.length; c++) {
-        if(newArr[c] === newArr[c + 1]) {
-            soma--
+        if(newArr[c + 1] === newArr[c]) { //REMOVE ITENS REPETIDOS
+            newArr.splice(newArr[c], 1)
+           
         }
     }
-    console.log(newArr)
-    console.log(cont)
-    console.log(soma)
-    return soma - (cont)
+    //console.log(qtd)
+    
+    return newArr.length - items -1
 }
 
-const lista = [1, 2, 3]
-
-
-
-
-
-console.log(somaIntervalos([[1, 4], [7, 10],[3, 5]]))
+console.log(somaIntervalos([[1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11]]))
