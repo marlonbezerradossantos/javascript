12f42017374b879e644b5e3bfe01edcc39eb3e70
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-11 17:56:07 -0300

    estudos do dia 11/09/24

--- exercicios/ex032/index.js
+++ exercicios/ex032/index.js
@@ -1,35 +1,22 @@
 // Escreva uma função que receba uma lista de arrays contendo intervalos entre dois números, retorne a soma destes interalos valores sobrepostos de intervalos devem ser contabilizados somente uma vez.
 
-function somaIntervalos(arr) {
-    let qtd = 0
-    items = arr.length
-    arr.map(function(x){//PREENCHE OS ARRAYS
-        const bckp = x.pop()
-        for(c = x[0] + 1; c < bckp; c++) {
-            x.push(c)
-        }
-        x.push(bckp)
-    })
-    const newArr = [...arr[0], ...arr[1], ...arr[2]] //CRIA UM ARRAY UNICO COM TODOS OS NUMEROS
-    for(c = 0; c < newArr.length; c++) { //ITERA SOBRE TODOS OS ITENS
-        if(newArr[c] > newArr[c + 1]) { // PÕE OS ITENS EM ORDEM
-            const bckp = newArr[c]
-            newArr[c] = newArr[c + 1]
-            newArr[c + 1] = bckp
-            c = 0
-        } 
-        if(newArr[c + 1] === newArr[c]) { //REMOVE ITENS REPETIDOS
-            newArr.splice(newArr[c], 1)
-           
-        }
-    }
-    //console.log(qtd)
-    
-    return newArr.length - items -1
-}
+// function somaIntervalos(arr) {
+//    console.log(arr)
+//    for(c = 0; c < arr.length; c++) {
+//     let bckp = c[0]
+//           // 0       // 1
+//     if(arr[0][1] > arr[0 + 1][1]) {
+//           // 0
+//         arr[0] = arr[0 + 1]
+//         arr[0 + 1] = arr[0]
+//         c = 0
+//     }
+//    }
+//    console.log(arr)
+// }
 
-console.log(somaIntervalos([[1, 5],
-    [10, 20],
-    [1, 6],
-    [16, 19],
-    [5, 11]]))
+// console.log(somaIntervalos([[1, 5],[2, 20],[3, 10]]))
+
+let lista = [[1], [2], [3]];
+lista[1] = lista[2];
+console.log(lista)
