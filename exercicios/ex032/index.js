033e2cadeb858295354babd0f17ad559e05f4852
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-23 17:34:59 -0300

    estudos do dia 23/09/24

--- exercicios/ex032/index.js
+++ exercicios/ex032/index.js
@@ -1,44 +1,19 @@
-// Escreva uma função que receba uma lista de arrays contendo intervalos entre dois números, retorne a soma destes interalos valores sobrepostos de intervalos devem ser contabilizados somente uma vez.
+// Escreva uma função que receba uma lista de arrays contendo intervalos entre dois números, retorne a soma destes intervalos valores sobrepostos de intervalos devem ser contabilizados somente uma vez.
 
 function somaIntervalos(arr) {
-    const repete = []
-    let newArr = []
-    
-    arr.map(function(x){
+
+    arr.map(function (x) {
         const bckp = x.pop()
-        for(c = x[0] + 1; c < bckp; c++){
+        for (c = x[0] + 1; c < bckp; c++) {
             x.push(c)
         }
         x.push(bckp)
     })
-    
-    
-//////////////////////////////
-    for(c of arr) {
-        for(i of c) {
-            newArr.push(i)
-        }
-    }
-    for(c = 0; c < newArr.length; c++) {
-        if(newArr[c] > newArr[c + 1]) {
-            const bckp = newArr[c]
-            newArr[c] = newArr[c + 1]
-            newArr[c + 1] = bckp
-            c = 0
-        }
-    }
+    //console.log(arr)
 
-    console.log(newArr)
     
-    for(c in newArr) {
-        for(i in arr) {
-            //console.log(`Comparando o Index: ${i} com o numero ${arr[i]} /// array completo = ${arr[i]}`)
-            if(arr[i].includes(newArr[c])) {
-                repete.includes(i) ? '' : repete.push(i)
-            } 
-        }
-    }
-    console.log(repete)
+    console.log(arr)
 }
 
-console.log(somaIntervalos([[1, 4],[3, 5], [8, 10]]))
+//console.log(somaIntervalos([[1, 4],[3, 5], [8, 10]]))
+console.log(somaIntervalos([[1, 4], [7, 10], [3, 5]])) // = 7
