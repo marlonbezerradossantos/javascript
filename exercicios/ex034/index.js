73ed08f8a80b2fbf02c8086a95d06e51ffba2ec3
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-09-27 17:38:09 -0300

    estudos do dia 27/09/24

--- exercicios/ex034/index.js
+++ exercicios/ex034/index.js
@@ -1,34 +1,18 @@
 // Crie uma função que receba uma função que terá uma string com um número no final. Sua função deverá ser capaz de incrementar o número contido nessa string 
 
 function incrementaString(str) {
-    let index;
+    let index = 0
     let newStr = []
     if(isNaN(Number(str[str.length - 1])) === true) {str = str + 0}
-
-
-    // for(letra in str) {
-    //     if(!isNaN(str[letra])) {
-    //         index = letra;
-    //         break
-    //     }
-    // }
-
-//////////////
-
-    for(c = str.length; c > -1; c--) {
-        if(!isNaN(str[c])) {
-            index = c;
+    for(c = (str.length - 1); c >= 0; c--) {
+        if(isNaN(str[c]) === true) {
+            index = c + 1
             break
         }
     }
-
-    console.log(index,'sou eu')
-
-//////////////
-
-    let num = str.slice(index, str.length)
-    for(c = 0; c < (str.length - num.length); c++) {
-        newStr.push(str[c])
+    let num = str.slice(index, str.length) 
+    for(c = 0; c < (str.length - num.length); c++) { 
+        newStr.push(str[c])                            
     }
     newStr = newStr.toString().split(',').join('')
     const tamanho = num.length
@@ -36,5 +20,8 @@ function incrementaString(str) {
     return newStr + num
 }
 
-console.log(incrementaString('Fo22obar99'))
-
+console.log(incrementaString('F22oobar99'))
+console.log(incrementaString('Foobar00'))
+console.log(incrementaString('Foobar0000'))
+console.log(incrementaString('Foobar71'))
+console.log(incrementaString('F22ooba3r100000'))
