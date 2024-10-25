8639ca258282fb813c703a5a09a6d566ebf67f51
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-10-25 18:34:31 -0300

    estudos do dia 25/10/24

--- aulas/aula33/index.js
+++ aulas/aula33/index.js
@@ -1,5 +1,7 @@
 // Estudo de Expressões Regulares - (RegEx)
 
-const texto = "1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 10000000"
-
-console.log(texto.match(/a*/gi))
+const texto = "25/10/24"
+const regex = /\d{2}/\d{2}/\d{4}/gi;
+//console.log(texto.match(/(Olá,) (meu nome)/gi))
+console.log(texto.replace(regex, "$2/$1/$3"))
+//console.log(texto)
