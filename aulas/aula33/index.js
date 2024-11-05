7c6ee3ecd4dae967fa06828738c3392235aaca7d
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2024-11-05 18:04:03 -0300

    estudos do dia 05/11/24

--- aulas/aula33/index.js
+++ aulas/aula33/index.js
@@ -1,9 +1,8 @@
 // Estudo de Expressões Regulares - (RegEx)
 
-// const nome = 'Marlon Bezerra dos Santos'
-// regex = /(([a-z]+) ([a-z]+)) ([a-z]+) ([a-z]+)/gi
-// const texto = nome.replace(regex, "$4 $5 $1 $2 $3 $1")
-// console.log(texto)
-const texto = "Essa piada foi muito boa ashuashuashuashuashua"
-console.log(texto.match(/\S+/g))
 
+const texto = "hello world!, world!"
+const regex = /world!/g
+console.log(regex.exec(texto))
+
+ n
