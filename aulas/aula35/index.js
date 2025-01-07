2f850f8717b5b728f0584a719c70c0664d175610
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-01-07 18:09:54 -0300

    estudos do dia 07/01/25

--- aulas/aula35/index.js
+++ aulas/aula35/index.js
@@ -33,11 +33,49 @@ numeros = numeros.filter(function(x) {
 
 // --> Reduce()
 
-numeros = [1, 2, 3, 4, 5] 
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] 
 numeros = numeros.reduce(function(prev, x) {
     return prev + x
 }, 0)
-console.log(numeros)
+//console.log(0, numeros)
+
+// --> Every()
+numeros = [2, 4, 6, 7]
+const saoPares = numeros.every(function(x){
+    return x % 2 === 0
+})
+//console.log(saoPares)
+
+// --> Some()
+
+numeros = [2, 4, 5, 6, 8]
+const temImpar = numeros.some(function(x){
+    return x % 2 === 1
+})
+//console.log(temImpar)
+
+// --> From()
+
+let str =  "Marlon"
+str = Array.from(str)
+//console.log(str)
+
+// --> With()
+
+nomes2 = nomes
+nomes2 = nomes.with(3, "Fulana")
+//console.log(nomes, nomes2)
+
+// --> Splice()
+
+let names = ["Marlon", "João", "José", "Geane"]
+names.splice(0, 2, "Fulano", "Cicrano")
+console.log(names)
+
+
+
+
+
 
 
 
