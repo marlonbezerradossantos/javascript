cb68c617ffa75bf30d5792e05201a51e49f82a70
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-01-09 17:31:50 -0300

    estudos do dia 09/01/25

---
+++ exercicios/ex042/index.js
@@ -0,0 +1,16 @@
+// let global = "Variável global"
+// function exibeVar() {
+//     let global = "Redeclaramos mas só vale aqui dentro"
+//     console.log(global)
+//     if(true) {
+//         let localDoIf = "Variável do somente do if"
+//     }
+//     console.log(localDoIf)
+// }
+// exibeVar()
+// console.log(local)
+// console.log(global)
+
+const arr = []
+arr[3] = 1
+console.log(arr)
