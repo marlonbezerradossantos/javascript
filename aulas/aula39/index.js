d7dfa7868200b6961c8c660b34ecdaede2c270bd
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-01-23 18:42:43 -0300

    estudos do dia 23/01/25

---
+++ aulas/aula39/index.js
@@ -0,0 +1,7 @@
+//a que n sei oq, n sei oq la
+"use strict"
+y = 10
+function sla(){
+    x = 10
+}
+console.log(y)
--- exercicios/ex045/index.js
+++ exercicios/ex045/index.js
@@ -1,14 +1,8 @@
 // crie duas funções, uma que codifique um número decimal em um número maia, e outro que codifique um número maia em um número decimal
 
 function paraDecimal(str) {
-
     str = str.split("\n")
-    const digitos = {
-        zero: /0/gi,
-        um: /./gi,
-        cinco: /-----/gi
-    }
-    const basesNumericas = [1, 20, 360, 7200, 144000]
+    const basesNumericas = [1, 20, 360, 7200, 144000, 2880000]
     let indiceBase = 0
     let resultado = 0;
     let contaNumeros = 0
@@ -33,10 +27,64 @@ function paraDecimal(str) {
     }
     return resultado += contaNumeros * basesNumericas[indiceBase]
 }
-
-//console.log(paraDecimal("  .  \n-----\n-----\n     \n   0   \n     \n-----"))
-
-console.log(paraDecimal("-----\n-----\n-----\n ... \n     \n-----\n-----\n  .  \n     \n-----\n     \n  0  \n     \n-----\n-----\n-----\n     \n-----\n.... "))
+//console.log(paraDecimal("  .  \n-----\n-----\n     \n   0   \n     \n-----")) //1811
 
 
+function paraMaia(num) {
+    const symbols = {
+        zero: "  0  ",
+        um: "  .  ",
+        dois: " ..  ",
+        tres: " ... ",
+        quatro: ".... ",
+        cinco: "-----",
+        pula: "     "
+    }
+    const bases = [1, 20, 360, 7200, 144000, 2880000]
+    let indiceBase = 0;
+    let resultado = 0;
+    let somaNums = 0
 
+    // while(somaNums < 359 && somaNums < num){
+    //     somaNums += 20
+    //     console.log(somaNums)
+    // }
+    // while(somaNums < 7199 && somaNums < num) {
+    //     somaNums += 360
+    //     console.log(somaNums)
+    // }
+    // while(somaNums < 143999 && somaNums < num) {
+    //     somaNums += 7200
+    //     console.log(somaNums)
+    
+    // } while(somaNums < 2879999 && somaNums < num) {
+    //     somaNums += 144000
+    //     console.log(somaNums)
+    // }
+
+
+    const breaksESomas = {
+        "359": 20,
+        "7199": 360,
+        "143999": 7200,
+        "2879999": 144000
+    }
+    controle = 0;
+    function acumula(quebra, soma) {
+        quebra = Number(quebra)
+        while(somaNums < quebra && somaNums < num) {
+            somaNums += soma
+            //console.log(somaNums)
+        }
+     
+        controle++
+        
+    }
+
+    for(c in breaksESomas) {
+        acumula(c, breaksESomas[c])
+    }
+}
+
+
+paraMaia(1794)
