334c8696f52a666f61de829e9d30d7b7290f70ce
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-02-14 17:47:29 -0300

    estudos do dia 14/02/25

--- exercicios/ex048/index.js
+++ exercicios/ex048/index.js
@@ -8,32 +8,59 @@ events: ["MFmf", "MmFfff", "1", "2"],
 expected: [[2,2], [5,5], [3,3], [0,0]] 
 */
 
+// bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
 function bathhouse(capacity, events) {
     const sections = [capacity, capacity]
+    const arrayGroups = []
+    let numberAssignment = 1
     for(let event of events) { //itera sobre todos os eventos
-        
+        let approved = true;
         const groups = {
-            "M": 0,
             "m": 0,
+            "f": 0,
+            "M": 0,
             "F": 0,
-            "f": 0
+            "number": 0
         }
-        if(event.includes("M", "F", "m", "f")) { //para eventos de entrada
-            for(let group in groups){//itera sobre o objeto de grupos e os eventos para contar
-                for(let people of event){  
-                    if(people === group){
+        
+        if(Number.isInteger(Number(event))) {
+            //identifica eventos de saída
+        } else {
+            //identifica eventos de entrada
+            for(let group in groups) {
+                for(let pessoa of event) {
+                    if(group === pessoa){
                         groups[group]++
                     }
                 }
             }
-            console.log(sections)
-            for(let group in groups) { //iteramos novamente no objeto para distribuir pessas
-                //provavelmente este for será inutil, precisaremos agora atender todas as condições para a entrada do grupo, se eles falharem então teremos que dar uma flag para rejeitar o grupo inteiro. Use um length para descobrir o numero total de pessoas e/ou o numero exato de pessoas do sexo masculino e feminino e então só com esta verificação preliminar conseguiremos rejeitar ou aceitar e seguir para outros requisitos de entrada, lembre-se que a entrada ou saída dependerão de um TRUE ou FALSE. Agora é contigo, já pensei o suficiente por hoje boa sorte.
-            }
-        } else { //para eventos de saída
             
-        }
+            
+            groups["number"] = numberAssignment
+            numberAssignment++
+            arrayGroups.push(groups)
+
+            // cada grupo é avaliado separadamente aqui(estamos lidando com um objeto aqui)
+            console.log('inicial: \nsecao:', sections[0], 'homens:',groups['m'] )
+            for(; groups['m'] > 0; groups['m']--){
+                sections[0]--
+                if(sections[0] < 0){
+                    approved = false
+                }
+
+                if(approved === false) {
+                    arrayGroups.pop()
+                }
+                // debugar este "for"
+            }
+     
+            
+        } 
     }
+    console.log(arrayGroups)
 }
 
-bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
+//bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
+bathhouse(5, ["mmmm", "mmmmmmm", "m", "1", "2"])
+//bathhouse(5, ["MFmf", "MmFfff", "mmmm", "FFFFF", "1", "2"])
+
---
+++ exercicios/ex048/tst.js
@@ -0,0 +1,4 @@
+let nums = ["m", "f", 'F', "M", '1']
+for(c of nums) {
+    console.log(!Number.isInteger(Number(c)))
+}
