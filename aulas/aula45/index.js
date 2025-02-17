493b373764bbea8b3f8286d9337eb793133cfdde
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-02-17 18:34:06 -0300

    estudos do dia 17/02/25

---
+++ aulas/aula45/index.js
@@ -0,0 +1,7 @@
+let elemento = document.querySelector("#title1")
+elemento.innerHTML = "ola mundo"
+
+let elementos = document.querySelectorAll('p')
+elementos[0].textContent = 'akak444akaakk'
+elementos[1].textContent = 'jajajj33ajajaj'
+elementos[2].textContent = 'whwhw55hhwhh'
--- exercicios/ex048/index.js
+++ exercicios/ex048/index.js
@@ -11,8 +11,9 @@ expected: [[2,2], [5,5], [3,3], [0,0]]
 // bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
 function bathhouse(capacity, events) {
     const sections = [capacity, capacity]
-    const arrayGroups = []
-    let numberAssignment = 1
+    
+    const capacityTotal = capacity * 2
+    let totalPeople = 0
     for(let event of events) { //itera sobre todos os eventos
         let approved = true;
         const groups = {
@@ -20,7 +21,6 @@ function bathhouse(capacity, events) {
             "f": 0,
             "M": 0,
             "F": 0,
-            "number": 0
         }
         
         if(Number.isInteger(Number(event))) {
@@ -35,32 +35,38 @@ function bathhouse(capacity, events) {
                 }
             }
             
+            // DAQUI PRA BAIXO SERÃO FEITAS AS VALIDAÇÕES
             
-            groups["number"] = numberAssignment
-            numberAssignment++
-            arrayGroups.push(groups)
-
-            // cada grupo é avaliado separadamente aqui(estamos lidando com um objeto aqui)
-            console.log('inicial: \nsecao:', sections[0], 'homens:',groups['m'] )
-            for(; groups['m'] > 0; groups['m']--){
-                sections[0]--
-                if(sections[0] < 0){
-                    approved = false
-                }
-
-                if(approved === false) {
-                    arrayGroups.pop()
-                }
-                // debugar este "for"
+            for(let iterateGroups in groups) {
+                totalPeople += groups[iterateGroups]
             }
-     
+
+            if(groups['M'] === 0 && groups['F'] === 0) { //Garantimos que há ao menos um 
+                approved = false                         //adulto para acompanhar crianças.
+            }
+
+            if(groups['M'] + groups['m'] > capacity || groups['F'] + groups['f'] > capacity) {
+                approved = false //Garantimos que há espaço para todos os grupos por genero
+            }
+             
+         
+            // separar homens e mulheres em um objeto? === [{homens}, {mulheres}]
+            // sempre refletir no array a soma dos generos? 
+            //iterar distribuir cada um segundo seu genero e o requerimento
             
+            
+            
+            
+            // se chegar aqui como FALSE o grupo será negado e nenhum número será atribuido
+            console.log(groups, ": ", approved)
         } 
+        console.log(sections)
     }
-    console.log(arrayGroups)
+    
 }
 
 //bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
-bathhouse(5, ["mmmm", "mmmmmmm", "m", "1", "2"])
+//bathhouse(5, ["mmmmM", "mmmmmmmM", "m", "1", "2"])
+bathhouse(5, ['MmmmFff'])
 //bathhouse(5, ["MFmf", "MmFfff", "mmmm", "FFFFF", "1", "2"])
 
