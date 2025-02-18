9da7b04b1544c9a605aeb39c78f489c97e1b8c96
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-02-18 18:33:29 -0300

    estudos do dia 18/02/25

--- aulas/aula45/index.js
+++ aulas/aula45/index.js
@@ -1,7 +1,2 @@
-let elemento = document.querySelector("#title1")
-elemento.innerHTML = "ola mundo"
-
-let elementos = document.querySelectorAll('p')
-elementos[0].textContent = 'akak444akaakk'
-elementos[1].textContent = 'jajajj33ajajaj'
-elementos[2].textContent = 'whwhw55hhwhh'
+let elemento = document.querySelector("#idmain div p")
+elemento.textContent = "deu certo né não?"
--- exercicios/ex048/index.js
+++ exercicios/ex048/index.js
@@ -10,9 +10,9 @@ expected: [[2,2], [5,5], [3,3], [0,0]]
 
 // bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
 function bathhouse(capacity, events) {
-    const sections = [capacity, capacity]
+    //const sections = [capacity, capacity]
     
-    const capacityTotal = capacity * 2
+    
     let totalPeople = 0
     for(let event of events) { //itera sobre todos os eventos
         let approved = true;
@@ -48,25 +48,58 @@ function bathhouse(capacity, events) {
             if(groups['M'] + groups['m'] > capacity || groups['F'] + groups['f'] > capacity) {
                 approved = false //Garantimos que há espaço para todos os grupos por genero
             }
+
+            
              
-         
+            const sections = [[groups['M'], groups['m']], [groups['F'], groups['f']]]
+            
+            //console.log(sections)
             // separar homens e mulheres em um objeto? === [{homens}, {mulheres}]
             // sempre refletir no array a soma dos generos? 
             //iterar distribuir cada um segundo seu genero e o requerimento
-            
-            
+            //console.log(sections)
+            const controle = []
+            function distribui(n) {
+                let distribute = 0
+
+                
+
+                if(sections[n][0] >= 1) {
+                    sections[n][0] -= 1
+                    distribute += 1
+                }
+
+                
+
+                //talvez fazer esta validação abaixo lá em cima seja o ideal
+                if(sections[n][0] + sections[n][1] > capacity) {
+                    console.log('deu ruim')
+                } else {
+                    distribute += sections[n][1]
+                    sections[n][1] -= sections[n][1]
+                    return distribute
+                }
+                
+            }
+
+            for(let gender in sections) {
+                controle.push(distribui(gender))
+            }
+            console.log(controle)
+
+        
             
             
             // se chegar aqui como FALSE o grupo será negado e nenhum número será atribuido
-            console.log(groups, ": ", approved)
+           
         } 
-        console.log(sections)
+       
     }
     
 }
 
 //bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
 //bathhouse(5, ["mmmmM", "mmmmmmmM", "m", "1", "2"])
-bathhouse(5, ['MmmmFff'])
+bathhouse(5, ['MmmmmmmmFfff'])
 //bathhouse(5, ["MFmf", "MmFfff", "mmmm", "FFFFF", "1", "2"])
 
