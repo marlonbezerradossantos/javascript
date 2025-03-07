01c99f44af223cef282aaeb8b154ebdea6842102
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-03-07 18:33:30 -0300

    estudos do dia 07/03/25

--- exercicios/ex048/index.js
+++ exercicios/ex048/index.js
@@ -1,147 +1,140 @@
 "use strict"
 // criar descrição dps tee hee
 
-/*
-name: "Family night. Jane, you go with dad and your little brother.",
-capacity: 5, 
-events: ["MFmf", "MmFfff", "1", "2"],
-expected: [[2,2], [5,5], [3,3], [0,0]] 
-*/
-
-// bathhouse(5, ["MFmf", "MmFfff", "1", "2"])
 function bathhouse(capacity, events) {
     let final = []
     const acceptedGroups = []
     const situation = [0, 0]
     let id = 1;
-    for(let event of events) { //itera sobre todos os eventos
+
+    //itera sobre todos os eventos
+    for(let event of events) { 
         
-        if(Number.isInteger(Number(event))) {
-            //identifica eventos de saída
-            for(let exit of acceptedGroups) {
-                if(exit.id === Number(event)) {
-                    situation[0] -= exit['M'] + exit['m']
-                    situation[1] -= exit['F'] + exit['f']
+        if(Number.isInteger(Number(event))) { //identifica eventos de saída
+            
+            for(let exitgroup of acceptedGroups) {
+                if(Number(event) === exitgroup.id) {
+                    situation[0] -= exitgroup['M'] + exitgroup['m']
+                    situation[1] -= exitgroup['F'] + exitgroup['f']
                 }
             }
-            
-            //console.log(acceptedGroups)
-        } else {
-            //identifica eventos de entrada
-            const groups = {
+
+        } else { //identifica eventos de entrada
+    
+            const atualGroup = {
                 "m": 0,
                 "f": 0,
                 "M": 0,
                 "F": 0,
-            }
-            let approved = true;
-            for(let group in groups) { //separamos crianças e adultos por genero em um objeto
-                for(let pessoa of event) {
-                    if(group === pessoa){
-                        groups[group]++
+            } 
+
+            //separamos crianças e adultos por genero em um objeto
+            let isApproved = true;
+            for(let gender in atualGroup) {
+                for(let peopleOfEvent of event) {
+                    if(gender === peopleOfEvent){
+                        atualGroup[gender]++
                     }
                 }
             }
-
-            
             
             // DAQUI PRA BAIXO SERÃO FEITAS AS VALIDAÇÕES
-            let totalPeople = 0
-            for(let iterateGroups in groups) { //aqui vemos quantas pessoas tentam entrar
-                totalPeople += groups[iterateGroups]
+
+            //aqui vemos quantas pessoas tentam entrar
+            let totalPeopleEntering = 0
+            for(let iterateActualGroup in atualGroup) { 
+                totalPeopleEntering += atualGroup[iterateActualGroup]
             }
             
-
             //aqui verificamos se há adulto do mesmo genero das crianças do grupo, se não houver e houver um adulto do genero oposto entao a criança é colocada junto do responsavel
-            if(groups['M'] === 0 && groups['m'] > 0) {
-                groups['f'] += groups['m']
-                groups['m'] = 0
-            } else if(groups['F'] === 0 && groups['f'] > 0) {
-                groups['m'] += groups['f'] 
-                groups['f'] = 0
+            if(atualGroup['M'] === 0 && atualGroup['m'] > 0) {
+                atualGroup['f'] += atualGroup['m']
+                atualGroup['m'] = 0
+            } else if(atualGroup['F'] === 0 && atualGroup['f'] > 0) {
+                atualGroup['m'] += atualGroup['f'] 
+                atualGroup['f'] = 0
             }
-
-           
-
-            if(groups['M'] === 0 && groups['F'] === 0) { //Garantimos que há ao menos um 
-                approved = false                         //adulto para acompanhar crianças.
-            }
-
-            const situationTotalSpace = situation[0] + situation[1]; 
-            if(situationTotalSpace + totalPeople > capacity * 2) { 
-                //garantimos que há espaço para todos nas seções independentemente de gênero
-                approved = false
-            }
-
             
-
-            if(groups['M'] + groups['m'] + situation[0] > capacity ||
-            groups['F'] + groups['f'] + situation[1] > capacity) {
-                //aqui verificamos se há espaço em cada seção por genero //xxxx
-                approved = false
-                console.log(approved)
-               
+            //Garantimos que há ao menos um adulto para acompanhar crianças.
+            if(atualGroup['M'] === 0 && atualGroup['F'] === 0) { 
+                isApproved = false                         
             }
 
+            //garantimos que há espaço para todos nas seções independentemente de gênero
+            const situationTotalPeople = situation[0] + situation[1]; 
+            if(situationTotalPeople + totalPeopleEntering > capacity * 2) { 
+                isApproved = false
+            }
 
-            const sections = [[groups['M'], groups['m']], [groups['F'], groups['f']]]
+            //veriificamos se o total de pessoas de um gênero ultrapassa a capacidade se uma seção, se sim e pudermos distribuir crianças para equilibrar então nós passamos elas para a outra seção contanto que haja um responsavel, caso contrario nós negamos o grupo.
+            if(atualGroup['M'] + atualGroup['m'] + situation[0] > capacity ||
+            atualGroup['F'] + atualGroup['f'] + situation[1] > capacity) {
+                const enteringGroupSituation = [atualGroup['M'] + atualGroup['m'], atualGroup['F'] + atualGroup['f']]
+                if(enteringGroupSituation[1] + situation[1] > capacity && atualGroup['M'] > 0) {
+                    const leftoverChild = enteringGroupSituation[1] + situation[1] - capacity
+                    atualGroup['f'] -= leftoverChild
+                    atualGroup['m'] += leftoverChild
+                } else if(enteringGroupSituation[0] + situation[0] > capacity && atualGroup['F'] > 0) {
+                    const leftoverChild = enteringGroupSituation[0] + situation[0] - capacity
+                    atualGroup['m'] -= leftoverChild
+                    atualGroup['f'] += leftoverChild
+                } else {
+                    isApproved = false
+                }
+            }
+
+            //criamos um array contendo sendo: [[homens, meninos], [mulheres, meninas]]
+            const sections = [[atualGroup['M'], atualGroup['m']], [atualGroup['F'], atualGroup['f']]]
             const gendersControl = []
             
             let childsLeftover = [0, 0];
+
+            //função que distribui as pessoas do grupo que tenta entrar
             function distribui(n) {
                 let distribute = 0
 
-                
-
-                if(sections[n][0] >= 1) {  //separamos 1 adulto para as crianças
+                //separamos 1 adulto para as crianças
+                if(sections[n][0] >= 1) {  
                     distribute += 1
                     sections[n][0] -= 1
                 }
 
+                //validamos se há espaço suficiente para uma seção por gênero e separamos as crianças que sobraram em uma variavel global
                 if(distribute + sections[n][1] > capacity) {
-                    //validamos se há espaço suficiente para uma seção por gênero e separamos as crianças que sobraram em uma variavel global
-                    //sections[n][1] -= (capacity - distribute);
                     childsLeftover[n] = (sections[n][1] + distribute) - 5
                     return 5
-                    
                 } else {
                     distribute += sections[n][0] + sections[n][1] 
                     return distribute
                 }
-                
             }
 
+            //atribuimos o grupo atual ao array a ser retornado
             for(let gender in sections) {
                 gendersControl.push(distribui(gender))
-                
             }
-            // agora distribuiremos os leftovers e se caso não houver jeito de equilibrar as seções, vamos dar um FALSE para o "approved" o que irá negar a entrada no grupo
+        
+            //distribuimos as crianças que sobraram em seus respectivos lugares
             if(childsLeftover[0] > 0) {
                 gendersControl[1] += childsLeftover[0] 
             } else if(childsLeftover[1] > 0) {
                 gendersControl[0] += childsLeftover[1]
             }
-            
-        
+
             // se chegar aqui como FALSE o grupo será negado e nenhum número será atribuido
-           if(approved === true) {
-                //console.log('grupo aceito:', gendersControl)
-                situation[0] += gendersControl[0]
-                situation[1] += gendersControl[1]
-                groups.id = id
-                id++
-                acceptedGroups.push(groups)
-                
-           }
+            if(isApproved === true) {
+                    situation[0] += gendersControl[0]
+                    situation[1] += gendersControl[1]
+                    atualGroup.id = id
+                    id++
+                    acceptedGroups.push(atualGroup)
+            }
         } 
-    final.push([situation[0], situation[1]])
+        final.push([situation[0], situation[1]])
     }
-    
-    
-    
     return final;
 }
 
-console.log(bathhouse(5, ["MFmf", "MmFfff", "1", "2"]))
-//                       [[2,2], [5,5], [3,3], [0,0]]  
+console.log(bathhouse(5, [["FFfF","1","MmffF","m","2","FFF","3","mfm","FMFMFFFF"]]))
+ 
 
